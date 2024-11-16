ecb68cebc8b7dd8744505693dd24b300f16b6539
PeterLevin511 <robotpetja@gmail.com>
2024-11-16 17:16:48 +0300

    cods

---
+++ Assets/Scrips/AnketaScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnketaScript : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    public TMP_Text nameTmpText;
+
+   public void OnButtonUserName() {
+        nameTmpText.text = nameInputField.text;
+   }    
+}
